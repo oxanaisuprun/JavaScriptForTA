9a4d19b691f06ca4bb97b5c288721e1883b2f13a
Oksana Suprun <osupru@softserveinc.com>
2022-09-24 14:53:09 +0300

    Changes to be committed: 	new file:   Hometask 1/Hometask1.html 	new file:   Hometask 1/javascript/index.js

---
+++ Hometask 1/javascript/index.js
@@ -0,0 +1,74 @@
+// // task 2
+
+// const lastName = "Suprun";
+// console.log(lastName);
+
+// ..............................................
+
+
+
+// // task 3
+// let firstVar = 42;
+// let secondVar = "The answer";
+// console.log(firstVar, secondVar);
+
+// firstVar = secondVar;
+// console.log(firstVar, secondVar);
+
+// ..............................................
+
+
+
+// // task 4
+// let fightDate;
+// const fighter = {
+//     nickname: "Rowdy",
+//     weight: 62,
+//     isDefited: true,
+//     latestFight: fightDate,
+//     win: null,
+//     bySplit: "win"*3,
+//     };
+// console.log(fighter);
+
+// ..............................................
+
+
+
+// // task 5
+// let isAdult = confirm("Are you older than 18?");
+// console.log(isAdult);
+
+// ..............................................
+
+
+
+// // task 6
+// const myName = "Oksana";
+// const myLastName = "Suprun";
+// const myGroup = "JS for TA";
+// const myBirthYear = 1989;
+// let  isMarried = true;
+
+// console.log(typeof myBirthYear, typeof isMarried, typeof myName);
+// let myCar = null;
+// let numberOfKids;
+
+// console.log(typeof numberOfKids, typeof myCar);
+
+// ..............................................
+
+
+
+// // task 7
+// let userLogin = prompt("Please enter your user login");
+// let userEmail = prompt("Please enter your e-mail");
+// let userPassword = prompt("Please enter your password");
+// console.log(`Dear ${userLogin}, your email is ${userEmail}, your password is ${userPassword}`);
+
+// ..............................................
+
+
+
+// // task 8
+// let currentTime = prompt("How time is it?", "HH:MM:SS");
