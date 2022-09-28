5e0ad83778b5ca5b621d0f5ef877464b7ae144a6
Oksana Suprun <oxanaisuprun@gmail.com>
2022-09-28 20:49:54 +0300

    Hometask 3 initiation

---
+++ Hometask 3/javascript/index.js
@@ -0,0 +1,74 @@
+// task 1
+// const myArray = [2, 3, 4, 5];
+// let n = 1;                                    // multiplier
+
+// for (let i = 0; i < myArray.length; i++){    // solution with FOR loop
+//     n*=myArray[i];
+// }
+// console.log(n);
+
+// let i = 0;                                   // solution with WHILE loop
+// while (i < myArray.length){
+//     n*=myArray[i];
+//     i++;
+// }
+// console.log(n);
+
+// ...........................................................................
+
+
+
+// task 2
+// for (let i = 0; i <=15; i++){
+//     if (i%2 == 0){
+//         console.log(`${i} is even`);
+//     } else {console.log(`${i} is odd`);}
+// }
+
+// ...........................................................................
+
+
+
+// task 3
+// function randArray(k) {
+// const myArray = [];
+
+//     for (let i = 0; i < k; i++){
+//         myArray[i] = Math.floor((Math.random() * 499) + 1);
+//     }
+//     return myArray
+// }
+// console.log(randArray(100));
+
+// ...........................................................................
+
+
+
+// task 4
+
+// function raiseToDegree(a, b){
+//     let res;
+//     return res = a**b;
+// }
+// let a = prompt("Enter a number to raise");
+// let b = prompt("Enter an exponentiation");
+// console.log(raiseToDegree(a,b));
+
+// ...........................................................................
+
+
+
+// task 5
+
+// let myArray = [12, 14, 4, -4, 0.2];
+// function findMin() {
+//     let newArray  = myArray.sort(function(a, b){return a - b});
+//     return newArray[0];
+// }
+// console.log(findMin());
+
+// ...........................................................................
+
+
+
+// task 6
