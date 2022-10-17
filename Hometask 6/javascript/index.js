3cd1f1749ff7d3edb9c23a92c812a52b8eab5075
Oksana Suprun <oxanaisuprun@gmail.com>
2022-10-17 21:02:56 +0300

    HW6 updates

--- Hometask 6/javascript/index.js
+++ Hometask 6/javascript/index.js
@@ -1,28 +1,77 @@
-// document.getElementById("test").innerHTML = "Last"; //1
+// Task 1
+// document.getElementById("test").innerHTML = "Last"; 
+// document.querySelector("div").innerHTML = "Last";
 
-// alert(document.getElementsByClassName("image").src = "cat.jpg"); //2
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// const nodeList = document.querySelectorAll("div>p"); //3
+
+
+
+// Task 2
+// alert(document.getElementsByClassName("image").src = "cat.jpg"); 
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+// Task 3
+// const nodeList = document.querySelectorAll("div>p"); 
 // for (let i = 0; i < nodeList.length; i++){
 //     console.log(`Selector text ${i}: ${nodeList[i].innerHTML}`);
 // }
 
-// alert(document.getElementById("list").children[0].innerText); //4
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+// Task 4
+// alert(document.getElementById("list").children[0].innerText); 
 // alert(document.getElementById("list").children[4].innerText);
 // alert(document.getElementById("list").children[1].innerText);
 // alert(document.getElementById("list").children[3].innerText);
 // alert(document.getElementById("list").children[2].innerText);
 
-document.querySelector("h1").style.backgroundColor = "#90EE90";
-document.querySelector("div>p").style.fontWeight = "bold";
-document.querySelector("div>p").nextElementSibling.style.color = "red";
-document.getElementById("myDiv").children[2].style.textDecoration = "underline";
-document.getElementById("myDiv").lastElementChild.style.fontStyle = "italic";
-// const myList = document.querySelectorAll("myList");
-// for (let i=0; i<myList.lenght; i++){
-//     let newArray = [];
-//     newArray.push(myList[i]);
-//     return newArray;
-// }
-// document.getElementById("myList").innerHTML = newArray;
-document.querySelector("span").style.visibility = "hidden";
+// alert(document.querySelector("#list").children[0].innerHTML);
+// alert(document.querySelector("#list").children[4].innerHTML);
+// alert(document.querySelector("#list").children[1].innerHTML);
+// alert(document.querySelector("#list").children[3].innerHTML);
+// alert(document.querySelector("#list").children[2].innerHTML);
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+// Task 5
+
+// document.querySelector("h1").style.backgroundColor = "#90EE90";
+// document.querySelector("div>p").style.fontWeight = "bold";
+// document.querySelector("div>p").nextElementSibling.style.color = "red";
+// document.getElementById("myDiv").children[2].style.textDecoration = "underline";
+// document.getElementById("myDiv").lastElementChild.style.fontStyle = "italic";
+// document.querySelector("span").style.visibility = "hidden";
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+// Task 6
+// let myInput1 = prompt("Input 1");
+// let myInput2 = prompt("Input 2");
+// document.getElementById("input1").value = myInput1;
+// document.getElementById("input2").value = myInput2;
+// function shuffleValues() {
+//     document.getElementById("input1").value = myInput2;
+//     document.getElementById("input2").value = myInput1;
+//   }
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  
+  
+  
+  
+  // Task 7
